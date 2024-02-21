0eab7210f00a842f2f3ff67fd4be9a3744bd1852
Ralu <->
2024-02-21 10:51:33 +0800

    [#] 灵猎技能调整

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -19370,7 +19370,10 @@ bool skill_check_condition_castbegin(map_session_data* sd, uint16 skill_id, uint
 			}
 			break;
 		case SOA_CIRCLE_OF_DIRECTIONS_AND_ELEMENTALS:
+			/*//KRO技改支持5连续放
 			if (!(sc && sc->getSCE(SC_T_FOURTH_GOD))) {
+			*/
+			if (!(sc && (sc->getSCE(SC_T_FOURTH_GOD) || sc->getSCE(SC_T_FIFTH_GOD)))) {
 				clif_skill_fail(sd,skill_id,USESKILL_FAIL_CONDITION,0);
 				return false;
 			}
